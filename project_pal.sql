77ddb7c32b2c1c726b86d1d2f46c1133f34f7690
spyan27 <147959212+spyan27@users.noreply.github.com>
2024-08-06 12:34:36 -0400

    updated tables added into the original SQL file

--- project_pal.sql
+++ project_pal.sql
@@ -1,16 +1,17 @@
 -- Create The project_pal schema
+DROP DATABASE IF EXISTS Project_pal;
 CREATE DATABASE IF NOT EXISTS Project_pal;
 USE Project_pal;
 
 -- Department Table
-CREATE TABLE Department (
+CREATE TABLE IF NOT EXISTS Department (
     dept_id INT AUTO_INCREMENT,
     deptName VARCHAR(75) NOT NULL,
     PRIMARY KEY (dept_id)
 );
 
 -- Professor Table
-CREATE TABLE Professor (
+CREATE TABLE IF NOT EXISTS Professor (
     professor_id INT AUTO_INCREMENT,
     first_name VARCHAR(100) NOT NULL,
     last_name VARCHAR(100) NOT NULL,
@@ -22,7 +23,7 @@ CREATE TABLE Professor (
 );
 
 -- Project Table
-CREATE TABLE Project (
+CREATE TABLE IF NOT EXISTS Project (
     project_id INT AUTO_INCREMENT,
     instructions TEXT,
     professor_id INT,
@@ -30,19 +31,43 @@ CREATE TABLE Project (
     PRIMARY KEY (project_id)
 );
 
+-- TA Table
+CREATE TABLE IF NOT EXISTS TA (
+    ta_id INT AUTO_INCREMENT,
+    first_name VARCHAR(75) NOT NULL,
+    last_name VARCHAR(75) NOT NULL,
+    email VARCHAR(75),
+    PRIMARY KEY (ta_id)
+);
+
+-- TA Speciality Table
+CREATE TABLE IF NOT EXISTS TASpeciality (
+    ta_id INT,
+    speciality_description VARCHAR(255),
+    FOREIGN KEY (ta_id) REFERENCES TA(ta_id)
+);
+
 -- Group Table
-CREATE TABLE `Group` (
+CREATE TABLE IF NOT EXISTS `Group` (
     group_id INT AUTO_INCREMENT,
     group_name VARCHAR(100) NOT NULL,
-    submission_id INT,
+    -- submission_id INT,
     ta_id INT, -- Assuming each group can have one TA
-    FOREIGN KEY (submission_id) REFERENCES Submission(submission_id),
     FOREIGN KEY (ta_id) REFERENCES TA(ta_id),
     PRIMARY KEY (group_id)
 );
 
+-- Submission Table
+CREATE TABLE IF NOT EXISTS Submission (
+    submission_id INT AUTO_INCREMENT,
+    group_id INT,
+    submitted_at DATETIME,
+    submission_link VARCHAR(100),
+    PRIMARY KEY (submission_id, group_id)
+);
+
 -- Student Table
-CREATE TABLE Student (
+CREATE TABLE IF NOT EXISTS Student (
     student_id INT AUTO_INCREMENT NOT NULL,
     first_name VARCHAR(75) NOT NULL,
     last_name VARCHAR(75) NOT NULL,
@@ -56,42 +81,14 @@ CREATE TABLE Student (
 );
 
 -- Student Speciality Table
-CREATE TABLE StudentSpeciality (
+CREATE TABLE IF NOT EXISTS StudentSpeciality (
     student_id INT,
     speciality_description VARCHAR(255),
     FOREIGN KEY (student_id) REFERENCES Student(student_id)
 );
 
--- TA Table
-CREATE TABLE TA (
-    ta_id INT AUTO_INCREMENT,
-    first_name VARCHAR(75) NOT NULL,
-    last_name VARCHAR(75) NOT NULL,
-    email VARCHAR(75),
-    group_id INT, -- Assuming each TA belongs to one group
-    FOREIGN KEY (group_id) REFERENCES `Group`(group_id),
-    PRIMARY KEY (ta_id)
-);
-
--- TA Speciality Table
-CREATE TABLE TASpeciality (
-    ta_id INT,
-    speciality_description VARCHAR(255),
-    FOREIGN KEY (ta_id) REFERENCES TA(ta_id)
-);
-
--- Submission Table
-CREATE TABLE Submission (
-    submission_id INT AUTO_INCREMENT,
-    group_id INT,
-    submitted_at DATETIME,
-    submission_link VARCHAR(100),
-    FOREIGN KEY (group_id) REFERENCES `Group`(group_id),
-    PRIMARY KEY (submission_id)
-);
-
 -- Availability Table
-CREATE TABLE Availability (
+CREATE TABLE IF NOT EXISTS Availability (
     availability_id INT AUTO_INCREMENT,
     student_id INT,
     ta_id INT,
@@ -101,33 +98,56 @@ CREATE TABLE Availability (
 );
 
 -- Days Table (Part of Availability Composite Attribute)
-CREATE TABLE Days (
+CREATE TABLE IF NOT EXISTS Days (
     day_id INT AUTO_INCREMENT,
     day ENUM('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday',
         'Saturday', 'Sunday') NOT NULL,
     availability_id INT,
-    FOREIGN KEY (availability_id) REFERENCES 
-Availability(availability_id),
+    FOREIGN KEY (availability_id) REFERENCES Availability(availability_id),
     PRIMARY KEY (day_id)
 );
 
 -- Time Table (Part of Availability Composite Attribute)
-CREATE TABLE Time (
+CREATE TABLE IF NOT EXISTS Time (
     time_id INT AUTO_INCREMENT,
     time TIME,
     availability_id INT,
-    FOREIGN KEY (availability_id) REFERENCES 
-Availability(availability_id),
+    FOREIGN KEY (availability_id) REFERENCES Availability(availability_id),
     PRIMARY KEY (time_id)
 );
 
 -- Location Table (Part of Availability Composite Attribute)
-CREATE TABLE Location (
+CREATE TABLE IF NOT EXISTS Location (
     location_id INT AUTO_INCREMENT,
     location VARCHAR(255) NOT NULL,
     availability_id INT,
-    FOREIGN KEY (availability_id) REFERENCES 
-Availability(availability_id),
+    FOREIGN KEY (availability_id) REFERENCES Availability(availability_id),
     PRIMARY KEY (location_id)
 );
 
+-- Class table
+CREATE TABLE IF NOT EXISTS Class (
+    course_name VARCHAR(55),
+    course_num INT,
+    CRN INT,
+    semester VARCHAR(25),
+    dept_id INT,
+    professor_id INT,
+    ta_id INT,
+    FOREIGN KEY (dept_id) REFERENCES Department(dept_id),
+    FOREIGN KEY (professor_id) REFERENCES Professor(professor_id),
+    FOREIGN KEY (ta_id) REFERENCES TA(ta_id),
+    PRIMARY KEY (CRN, semester)
+);
+
+-- Section table
+CREATE TABLE IF NOT EXISTS Section (
+    CRN INT,
+    semester VARCHAR(25),
+    section_id INT,
+    course_name VARCHAR(55),
+    professor_id INT,
+    PRIMARY KEY (CRN, semester, section_id),
+    FOREIGN KEY (CRN, semester) REFERENCES Class(CRN, semester),
+    FOREIGN KEY (professor_id) REFERENCES Professor(professor_id)
+);
